3f3fd75d7d9f83bb2382538f57880d58dc167415
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2024-02-06 17:55:31 -0500

    Add corsheaders

--- mvp_recongnition/settings.py
+++ mvp_recongnition/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = 'django-insecure-zmf2fo6s)cpa_h-$8hz$%rbpr2+x(whissux8!l7(+jbw6@jt^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000"]
 
 # Application definition
 
@@ -35,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'recognition',
 
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
